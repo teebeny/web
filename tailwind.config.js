bc63e673db7f775756fba4ced783f97723c78fe3
teebeny <86422140+teebeny@users.noreply.github.com>
2022-06-28 22:46:42 +0100

    made changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    fontFamily: {
+      Outfit: ["'Outfit'", "sans-serif"],
+    },
+
     extend: {
       scale: {
-        '103': '1.03',
-      }
+        103: "1.03",
+      },
     },
   },
   plugins: [],
-}
+};
