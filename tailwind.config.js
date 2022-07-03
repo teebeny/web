927cd3c9ac881b3a0e1775232c5d305a31ec5ca5
teebeny <86422140+teebeny@users.noreply.github.com>
2022-07-03 16:47:06 +0100

    edit2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    fontFamily: {
-      Outfit: ["'Outfit'", "sans-serif"],
-    },
-
     extend: {
       scale: {
         103: "1.03",
       },
+      fontFamily: {
+        OpenSans: ["'Open sans'", "sans-serif"],
+      },
+      height: {
+        600: "37.5rem",
+        700: "40rem",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
